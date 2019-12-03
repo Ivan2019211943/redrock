ea1c40a71c489b72665ba2c4759817c7d5f77c24
Ivan2019211943 <56482219+Ivan2019211943@users.noreply.github.com>
2019-12-04 00:15:16 +0800

    Update 廖成伟-2019211943-level 1.go

--- test/廖成伟-2019211943-level 1.go
+++ test/廖成伟-2019211943-level 1.go
@@ -1,1 +1,28 @@
-package awesomeProject1
+func Register(c *gin.Context) {
+//获取用户名、密码
+name := c.Request.FormValue("Name")
+passwd := c.Request.FormValue("Passwd")
+//判断用户是否存在
+//不存在创建用户，保存密码与用户名
+Bool := Func.IsExist(name)
+if Bool {
+//注册状态
+State["state"] = 1
+State["text"] = "此用户已存在！"
+} else {
+//用户不存在即添加用户
+AddStruct(name, passwd)
+State["state"] = 1
+State["text"] = "注册成功！"
+}
+//登录
+func Login(c *gin.Context) {
+name := c.Request.FormValue("Name")
+passwd := c.Request.FormValue("Passwd")
+if Bool_Pwd {
+State["state"] = 1
+State["text"] = "登录成功！"
+} else {
+State["state"] = 0
+State["text"] = "密码错误！"
+}
